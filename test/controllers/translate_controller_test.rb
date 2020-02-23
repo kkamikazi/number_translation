60b64557a64e6f744d51374208385e3c2b44b075
Cassio Maletich <cgm1010@gmail.com>
2020-02-23 18:25:57 -0300

    fix assert order

--- test/controllers/translate_controller_test.rb
+++ test/controllers/translate_controller_test.rb
@@ -4,66 +4,66 @@ class TranslateControllerTest < ActionDispatch::IntegrationTest
   test "translate 0" do
     get "/0"
     result = JSON.parse(@response.body)
-    assert_equal result["extenso"], "zero"
+    assert_equal "zero", result["extenso"]
   end
 
   test "translate 1" do
     get "/1"
     result = JSON.parse(@response.body)
-    assert_equal result["extenso"], "um"
+    assert_equal "um", result["extenso"]
   end
 
   test "translate -1" do
     get "/-1"
     result = JSON.parse(@response.body)
-    assert_equal result["extenso"], "menos um"
+    assert_equal "menos um", result["extenso"]
   end
 
   test "translate 17" do
     get "/17"
     result = JSON.parse(@response.body)
-    assert_equal result["extenso"], "dezessete"
+    assert_equal "dezessete", result["extenso"]
   end
 
   test "translate -13" do
     get "/-13"
     result = JSON.parse(@response.body)
-    assert_equal result["extenso"], "menos treze"
+    assert_equal "menos treze", result["extenso"]
   end
 
   test "translate 37" do
     get "/37"
     result = JSON.parse(@response.body)
-    assert_equal result["extenso"], "trinta e sete"
+    assert_equal "trinta e sete", result["extenso"]
   end
 
   test "translate -79" do
     get "/-79"
     result = JSON.parse(@response.body)
-    assert_equal result["extenso"], "menos setenta e nove"
+    assert_equal "menos setenta e nove", result["extenso"]
   end
 
   test "translate -100" do
     get "/-100"
     result = JSON.parse(@response.body)
-    assert_equal result["extenso"], "menos cem"
+    assert_equal "menos cem", result["extenso"]
   end
 
   test "translate -879" do
     get "/-879"
     result = JSON.parse(@response.body)
-    assert_equal result["extenso"], "menos oitocentos e setenta e nove"
+    assert_equal "menos oitocentos e setenta e nove", result["extenso"]
   end
 
   test "translate -900" do
     get "/-900"
     result = JSON.parse(@response.body)
-    assert_equal result["extenso"], "menos novecentos"
+    assert_equal "menos novecentos", result["extenso"]
   end
 
   test "translate 918" do
     get "/918"
     result = JSON.parse(@response.body)
-    assert_equal result["extenso"], "novecentos e dezoito"
+    assert_equal "novecentos e dezoito", result["extenso"]
   end
 end
